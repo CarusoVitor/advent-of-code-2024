b1a6a00a88fedf8118396f45d01a9ebc1959dc10
Vitor Caruso <crfvitor8d@hotmail.com>
2024-12-05 23:12:44 -0300

    multiover (2): read do and don't indexes

--- advent/3multitover/multiover.go
+++ advent/3multitover/multiover.go
@@ -12,6 +12,9 @@ var multRegex = regexp.MustCompile(`mul\((?P<left>\d+),(?P<right>\d+)\)`)
 var leftIdx = multRegex.SubexpIndex("left")
 var rightIdx = multRegex.SubexpIndex("right")
 
+var doRegex = regexp.MustCompile(`do\(\)`)
+var dontRegex = regexp.MustCompile(`don't\(\)`)
+
 func readFile(path string) string {
 	file, err := os.Open(path)
 	if err != nil {
@@ -27,48 +30,97 @@ func readFile(path string) string {
 type mult struct {
 	left  int
 	right int
-	value int
+	index int
 }
 
-func newMult(left, right int) mult {
-	value := left * right
-	return mult{left, right, value}
+type do struct {
+	neg   bool
+	index int
+}
+
+func newDo(neg bool, index int) do {
+	return do{neg, index}
+}
+
+func newMult(left, right, index int) mult {
+	return mult{left, right, index}
 }
 
 func findMultMatches(text string) ([]mult, error) {
 	mult := make([]mult, 0, 256)
 	matches := multRegex.FindAllStringSubmatch(text, -1)
+	indexes := multRegex.FindAllStringSubmatchIndex(text, -1)
 
-	for _, match := range matches {
-		left := match[leftIdx]
+	if len(matches) != len(indexes) {
+		panic(
+			fmt.Sprintf("matches must has len %d different than index (%d)", len(matches), len(indexes)),
+		)
+	}
+
+	for idx := range matches {
+		left := matches[idx][leftIdx]
 		leftValue, err := strconv.Atoi(left)
 		if err != nil {
 			return nil, err
 		}
 
-		right := match[rightIdx]
+		right := matches[idx][rightIdx]
 		rightValue, err := strconv.Atoi(right)
 		if err != nil {
 			return nil, err
 		}
-		multObj := newMult(leftValue, rightValue)
+		index := indexes[idx][0]
+		multObj := newMult(leftValue, rightValue, index)
 		mult = append(mult, multObj)
 	}
 	return mult, nil
 }
 
-func parseInput(text string) []mult {
-	matches, err := findMultMatches(text)
+func findDoMatches(text string) ([]do, error) {
+	do := make([]do, 0, 256)
+	indexes := doRegex.FindAllStringSubmatchIndex(text, -1)
+
+	for _, indexSlice := range indexes {
+		index := indexSlice[0]
+		doObj := newDo(false, index)
+		do = append(do, doObj)
+	}
+	return do, nil
+}
+
+func findDontMatches(text string) ([]do, error) {
+	dont := make([]do, 0, 256)
+	indexes := dontRegex.FindAllStringSubmatchIndex(text, -1)
+
+	for _, indexSlice := range indexes {
+		index := indexSlice[0]
+		dontObj := newDo(true, index)
+		dont = append(dont, dontObj)
+	}
+	return dont, nil
+}
+
+func parseInput(text string) ([]mult, []do, []do) {
+	mults, err := findMultMatches(text)
 	if err != nil {
 		panic(err)
 	}
-	return matches
+	do, err := findDoMatches(text)
+	if err != nil {
+		panic(err)
+	}
+	dont, err := findDontMatches(text)
+	if err != nil {
+		panic(err)
+	}
+
+	return mults, do, dont
 }
 
 func sumMultsResults(mults []mult) int {
 	sum := 0
 	for _, multObj := range mults {
-		sum += multObj.value
+		sum += multObj.left * multObj.right
 	}
 	return sum
 }
@@ -77,7 +129,7 @@ func MultiOverPartOne() {
 	path := "advent/3multitover/input.txt"
 	text := readFile(path)
 
-	mults := parseInput(text)
+	mults, _, _ := parseInput(text)
 	result := sumMultsResults(mults)
 	fmt.Printf("The answer is %d\n", result)
 }
--- advent/3multitover/multiover_test.go
+++ advent/3multitover/multiover_test.go
@@ -1,21 +1,50 @@
 package advent
 
 import (
-	"bufio"
 	"reflect"
-	"strings"
 	"testing"
 )
 
 func TestFindMultMatches(t *testing.T) {
-	scanner := bufio.NewScanner(strings.NewReader("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"))
+	text := "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
 	expected := []mult{
-		newMult(2, 4),
-		newMult(5, 5),
-		newMult(11, 8),
-		newMult(8, 5),
+		newMult(2, 4, 1),
+		newMult(5, 5, 29),
+		newMult(11, 8, 53),
+		newMult(8, 5, 62),
 	}
-	result := parseInput(scanner)
+	result, err := findMultMatches(text)
+	if err != nil {
+		t.Error(err)
+	}
+	if !reflect.DeepEqual(expected, result) {
+		t.Errorf("result %#v != expected %#v", result, expected)
+	}
+}
+
+func TestFindDoMatches(t *testing.T) {
+	text := "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+	expected := []do{
+		newDo(false, 59),
+	}
+	result, err := findDoMatches(text)
+	if err != nil {
+		t.Error(err)
+	}
+	if !reflect.DeepEqual(expected, result) {
+		t.Errorf("result %#v != expected %#v", result, expected)
+	}
+}
+
+func TestFindDontMatches(t *testing.T) {
+	text := "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+	expected := []do{
+		newDo(true, 20),
+	}
+	result, err := findDontMatches(text)
+	if err != nil {
+		t.Error(err)
+	}
 	if !reflect.DeepEqual(expected, result) {
 		t.Errorf("result %#v != expected %#v", result, expected)
 	}
